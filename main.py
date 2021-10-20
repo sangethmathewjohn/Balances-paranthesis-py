d90a9ea92ac9a6d44c8125999653472a9ccc5811
Sangeeth Mathew John <65032296+sangethmathewjohn@users.noreply.github.com>
2021-10-20 21:53:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,21 +5,25 @@ close_list = ["]","}",")"]
 # Function to check parentheses
 def check(myStr):
     stack= []
+    # converting string into a stack
     for i in myStr:
         stack.append(i)
     print(len(stack))
-          
+    if len(stack)%2==1:
+        return "Unbalanced"
     while not (len(stack)==0):
-        if stack[len(stack)-1] in open_list:
-            poso =open_list.index(stack[len(stack)-1])
+        i=len(stack)-1
+        x=stack[i]
+        if x in open_list:
+            poso =open_list.index(x)
             if close_list[poso] in stack:
                 stack.pop(stack.index(close_list[poso]))
                 pos=stack.index(open_list[poso])
                 stack.pop(pos)
             else:
                 return "Unbalnced"
-        elif stack[len(stack)-1] in close_list:
-            poso =close_list.index(stack[len(stack)-1])
+        elif x in close_list:
+            poso =close_list.index(x)
             if open_list[poso] in stack:
                 stack.pop(stack.index(open_list[poso]))
                 pos=stack.index(close_list[poso])
